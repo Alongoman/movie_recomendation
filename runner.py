54e2643128804dba8f7883da38b8cc3375508043
yogevhad <yogevhad987@gmail.com>
2022-02-09 10:33:44 +0200

    mac

--- runner.py
+++ runner.py
@@ -1,11 +1,11 @@
-from movie_recomendation import trainer
+import trainer
 
 
 lr = [5e-3,1e-3,5e-4]
-batch_size = [100]#,300,500]
-epochs = 25
+batch_size = [300]#,300,500]
+epochs = 30
 topk = 10
-embedding_size = [16,32,64]
+embedding_size = [32]
 num_layers = [3,4,5]
 save_weights = True
 show_graph = False
