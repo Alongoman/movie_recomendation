839ae6d8441aa77ccfbb5b1d773e8b5b70118a78
Alongoman <alon.goldmann@gmail.com>
2022-02-09 10:24:56 +0200

    fix graphs save over each other

--- runner.py
+++ runner.py
@@ -2,10 +2,10 @@ from movie_recomendation import trainer
 
 
 lr = [5e-3,1e-3,5e-4]
-batch_size = [100]#,300,500]
-epochs = 25
+batch_size = [400]#,300,500]
+epochs = 3
 topk = 10
-embedding_size = [16,32,64]
+embedding_size = [32,64]
 num_layers = [3,4,5]
 save_weights = True
 show_graph = False
