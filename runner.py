98c49929a68aa88c3b0721f0ecec0676d76e791a
Alongoman <alon.goldmann@gmail.com>
2022-02-15 10:43:59 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -7,6 +7,7 @@ epochs = 30
 topk = 10
 embedding_size = [32]
 num_layers = [3,4]
+dropout = 0.5
 generate_new_data = False
 save_weights = True
 show_graph = False
@@ -20,6 +21,6 @@ for l in lr:
     for b in batch_size:
         for e in embedding_size:
             for n in num_layers:
-                trainer.Train(lr=l, batch_size=b, embedding_size=e, epochs=epochs, top_k=topk, num_layers=n,
+                trainer.Train(lr=l, dropout=dropout, batch_size=b, embedding_size=e, epochs=epochs, top_k=topk, num_layers=n,
                               save_weights=save_weights, show_graph=show_graph, save_graphs=save_graphs, use_avg_rating=use_avg_rating, generate_new_data=generate_new_data)
 
